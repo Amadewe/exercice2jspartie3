8e8ed0a1275ac1d0cef0d90a2a8a947418216da9
Amadewe <dewevre.amandine@gmail.com>
2019-01-29 13:59:37 +0100

    Ajout des commentaires

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
+// afficher ou masquer le texte en fonction de l'ancre.
+// 1ere fonction pour afficher
+// La propriété display définit le type d'affichage utilisée pour le rendu d'un élément
 function Display(){
 document.getElementById('text').style.display = 'block';
 }
+// 2eme fonction pour Hide
 function Hide(){
+// en mettant comme valeur 'none' à la propriété display l'élément ne s'affiche pas.
 document.getElementById('text').style.display = 'none';
 }
